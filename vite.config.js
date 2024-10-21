75d435e05a3e1b8a2b3c29add41855a721e771d2
Florencia Del Castillo <florfvidela@gmail.com>
2024-10-21 20:34:20 -0300

    v1.1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'https:/Valentin-G24.github.io/Valentin-G24.github.io'
 })
